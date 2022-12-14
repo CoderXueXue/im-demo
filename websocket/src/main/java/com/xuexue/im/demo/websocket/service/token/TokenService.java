15c51b3e9977ab98e7d2889f8ed1b411b342f9cd
huchai <huchai@quthing.com>
2022-12-14 16:04:39 +0800

    update Token 服务

--- websocket/src/main/java/com/xuexue/im/demo/websocket/service/token/TokenService.java
+++ websocket/src/main/java/com/xuexue/im/demo/websocket/service/token/TokenService.java
@@ -2,14 +2,25 @@ package com.xuexue.im.demo.websocket.service.token;
 
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author huchai
  * @date 2022/12/14
+ * @description token服务
  */
 @Service
 public class TokenService {
 
+    private static final Map<String, Integer> tokenMap = new HashMap<>();
 
+    static {
+        tokenMap.put("uuuuuaaaaaa", 1);
+        tokenMap.put("qweadsadasd", 2);
+    }
 
-
+    public Integer getUserIdByToken(String token) {
+        return tokenMap.get(token);
+    }
 }
